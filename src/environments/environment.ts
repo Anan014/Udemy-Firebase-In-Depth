08281cca45cd3f289617fd55bb069196ddddaad9
Anan014 <ananamer95@gmail.com>
2022-11-19 10:52:33 +0200

    Section 1: Introduction

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-
+    apiKey: "AIzaSyD4cAYLv6weiGlhRk05oP06Le8tnMkL0D4",
+    authDomain: "fir-course-recording-bd204.firebaseapp.com",
+    projectId: "fir-course-recording-bd204",
+    storageBucket: "fir-course-recording-bd204.appspot.com",
+    messagingSenderId: "642287756984",
+    appId: "1:642287756984:web:c828eacb91fd7cffc7364d",
   },
-  api: {
-
-  }
+  api: {},
 };
 
 /*
@@ -19,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+import "zone.js/plugins/zone-error"; // Included with Angular CLI.
